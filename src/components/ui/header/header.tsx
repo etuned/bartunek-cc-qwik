8f609a6603e1748f42ef1290130c04cf3ea2a5b6
Edwin Bartunek <116134167+ebartunek@users.noreply.github.com>
2023-05-09 02:01:39 +0900

    use Link

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,45 +1,51 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
-import { LogoIcon } from '../icons/logo';
-import { LinkedinIcon } from '../icons/linkedin';
-import { GithubIcon } from '../icons/github';
-import styles from './header.scss?inline';
+import { component$, useStyles$ } from "@builder.io/qwik";
+import { LogoIcon } from "../icons/logo";
+import { LinkedinIcon } from "../icons/linkedin";
+import { GithubIcon } from "../icons/github";
+import styles from "./header.scss?inline";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-    useStyles$(styles);
+  useStyles$(styles);
   return (
     <nav>
-    <ul>
+      <ul>
         <li>
-            <h1>
-                <a href="/">
-                    <span aria-hidden="true">
-                        <LogoIcon />
-                    </span>
-                    <span class="sr-only">Home</span>
-                </a>
-            </h1>
-        </li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/blog">Blog</a></li>
-        <li>
-            <a href="https://www.linkedin.com/in/ebartunek/" target="_blank">
-                <span aria-hidden="true">
-                    <LinkedinIcon />
-                </span>
-                <span class="sr-only">LinkedIn</span>
-            </a>
+          <h1>
+            <Link href="/">
+              <span aria-hidden="true">
+                <LogoIcon />
+              </span>
+              <span class="sr-only">Home</span>
+            </Link>
+          </h1>
         </li>
         <li>
-            <a href="https://www.github.com/etuned">
-                <span aria-hidden="true">
-                    <GithubIcon />
-                </span>
-                <span class="sr-only">Github</span>
-            </a>
+          <Link href="/projects">Projects</Link>
         </li>
-    </ul>
-</nav>
-
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/ebartunek/" target="_blank">
+            <span aria-hidden="true">
+              <LinkedinIcon />
+            </span>
+            <span class="sr-only">LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.github.com/etuned">
+            <span aria-hidden="true">
+              <GithubIcon />
+            </span>
+            <span class="sr-only">Github</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 });
