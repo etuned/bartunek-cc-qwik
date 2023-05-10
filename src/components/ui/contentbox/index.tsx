9403bad90cb12bf234d53f066906676c8aa2994f
Edwin Bartunek <116134167+ebartunek@users.noreply.github.com>
2023-05-11 01:25:30 +0900

    update contentbox

--- src/components/ui/contentbox/index.tsx
+++ src/components/ui/contentbox/index.tsx
@@ -1,13 +1,16 @@
-import { Slot, component$, useStyles$ } from '@builder.io/qwik';
-import styles from './contentbox.scss?inline';
+import { Slot, component$, useStyles$ } from "@builder.io/qwik";
+import styles from "./contentbox.scss?inline";
 
 export default component$(() => {
-  useStyles$(styles)
+  useStyles$(styles);
   return (
     <div>
       <h3 class="title">
         <Slot name="title" />
       </h3>
+      <div class="centered">
+        <Slot name="centered" />
+      </div>
       <Slot />
     </div>
   );
