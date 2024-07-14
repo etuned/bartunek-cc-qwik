9aba20f3604cda3f9245017b250f7d9e1e4df4bd
Edwin Bartunek <edwin@devhart.io>
2024-07-14 23:27:49 +0900

    upgrade packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import { StorybookConfig } from 'storybook-framework-qwik';
 
 const config: StorybookConfig = {
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook'
+  ],
   framework: {
     name: 'storybook-framework-qwik',
   },
